56e704cf9378ba0acdd806d0184ab647edb5032e
Kolmakov Mikhail <kolmakovmi@mail.cbr.ru>
2022-03-30 00:27:09 +0300

    fix assert

--- test_items.py
+++ test_items.py
@@ -6,4 +6,4 @@ def test_add_to_basket_btn_is_present(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     time.sleep(30)
     add_to_basket_button = browser.find_elements(By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary.btn-add-to-basket")
-    assert len(add_to_basket_button) is not None, "Add to basket button is not displayed"
+    assert add_to_basket_button, "Add to basket button is not displayed"
